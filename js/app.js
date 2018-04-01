81307b221598d3a5f055cd69d22c8f5862939a96
Christine Reffo <christine.reffo@gmail.com>
2018-03-31 18:00:44 -0700

    Deck function updated and restart button started git status

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 /*
  * Create a list that holds all of your cards
  */
+
 const cardDeck = document.querySelector('.deck');
 const allCards = document.querySelectorAll('.card');
 const allSymbols = document.querySelectorAll('.i');
@@ -24,40 +25,9 @@ for(let i = 0; i<allSymbols.length; i++) {
  */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
-function makeDeck(cardArray) {
-  const fragment = document.createDocumentFragment();  // ← uses a DocumentFragment instead of a <div>
-
-  cardDeck.remove('i');
-  document.querySelector('.score-panel').insertAdjacentHTML('afterend', "<ul class = 'deck'></ul>");
-
-    for (let i = 0; i < cardArray[i].length; i++) {
-
-          let cardHTML = cardArray[i].outerHTML;
-
-          fragment.inserAdjacentHTML(cardHTML);
 
 
-          // let cardHTML = document.createElement('li');
-
-//           cardHTML.className('card');
-//
-//           fragment.appendChild(cardHTML)
-// }
-//           cardDeck.appendChild(fragment);
-//
-//     for(let j = 0; j < cardArray[i].length; j++) {
-//
-//           let symbolHTML = document.createElement('i');
-//           let symbol = 'symbolArray[i].className';
-//
-//
-//           symbolHTML.className(symbol);
-//
-//           fragment.find('li').last().append(symbolHTML);
-//
-//             }
-//
-// }
+// Issues with identifiers, work only sporadically
 
 
 function shuffle(cardArray) {
@@ -72,21 +42,42 @@ function shuffle(cardArray) {
     }
 
     return cardArray;
-    makeDeck(cardArray, symbolArray);
+    makeDeck(cardArray);
 }
 
 
 
 // needs to be tested in console --> only remakes the deck not the cards;
+function makeDeck(cardArray) {
+  const fragment = document.createDocumentFragment();  // ← uses a DocumentFragment instead of a <div>
+
+// remove the HTML structure of the cards in order to add the shuffled ones
+  cardDeck.remove('i');
+
+// by removing the i tags, the entire list container is removed as well, so I need to add that back in
+  document.querySelector('.score-panel').insertAdjacentHTML('afterend', "<ul class = 'deck'></ul>");
+
+//  looping through the array of cards to extract the HTML structure including the symbol, to then add it to the HTML
+    for (let i = 0; i < cardArray[i].length; i++) {
+
+          let cardHTML = cardArray[i].outerHTML;
+
+          fragment.insertAdjacentHTML('beforeend', "cardHTML");
+        }
+
+      document.cardDeck.appendChild(fragment);
+
+}
 
 
 
 
+// reset grid on click !!! ONLY CONSOLE LOG TEST WORKS at the moment
+let restart = document.querySelector('.fa-repeat')
 
-// reset grid on click
-let restart = document.querySelector('.restart')
-
-restart.addEventListener('click', shuffle());
+restart.addEventListener('click', function shuffle(cardArray) {
+  console.log("the button was clicked");
+});
 
 
 
